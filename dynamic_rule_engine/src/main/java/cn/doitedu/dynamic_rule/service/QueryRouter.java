6ec1d180cc215c51a8fd253c416b92f5fa8957e4
83544844@qq.com <123456qb>
2021-03-31 12:44:17 +0800

    整理注释

--- dynamic_rule_engine/src/main/java/cn/doitedu/dynamic_rule/service/QueryRouter.java
+++ dynamic_rule_engine/src/main/java/cn/doitedu/dynamic_rule/service/QueryRouter.java
@@ -98,7 +98,7 @@ public class QueryRouter {
         }
 
         /**
-         * 近期条件组查询
+         * 近期条件组，批量打包查询
          */
         if (nearRangeParams.size() > 0) {
             // 将规则总参数对象中的“次数类条件”覆盖成： 近期条件组
@@ -109,7 +109,7 @@ public class QueryRouter {
         }
 
         /**
-         * 远期条件组查询
+         * 远期条件组，批量打包查询
          */
         if (farRangeParams.size() > 0) {
             // 将规则总参数对象中的“次数类条件”覆盖成： 远期条件组
@@ -121,10 +121,8 @@ public class QueryRouter {
 
         /**
          * 跨界count条件组查询
+         * 叶子条件逐一控制查询
          */
-        RuleParam copyParam1 = new RuleParam(); // 分界点右边分段的count参数
-        RuleParam copyParam2 = new RuleParam(); // 分界点左边分段的count参数
-
         for (RuleAtomicParam crossRangeParam : crossRangeParams) {
             long originRangeStart = crossRangeParam.getRangeStart();
 
@@ -148,59 +146,82 @@ public class QueryRouter {
      * 控制sequence类条件查询路由
      */
     public boolean sequenceConditionQuery(LogBean logBean, RuleParam ruleParam, ListState<LogBean> eventState) throws Exception {
-
-        // 计算查询分界点timestamp
-        // 当前时间对小时取整，-1
-        long splitPoint = DateUtils.addHours(DateUtils.ceiling(new Date(), Calendar.HOUR), -2).getTime();
-
         // 取出规则中的序列条件
         List<RuleAtomicParam> userActionSequenceParams = ruleParam.getUserActionSequenceParams();
 
-        // 取出规则中的序列的总步骤数
-        int totalSteps = userActionSequenceParams.size();
-
         // 如果序列条件有内容，才开始计算
         if(userActionSequenceParams!=null && userActionSequenceParams.size()>0){
+            // 计算查询分界点timestamp ：当前时间对小时取整，-1
+            long splitPoint = DateUtils.addHours(DateUtils.ceiling(new Date(), Calendar.HOUR), -2).getTime();
+
+            // 取出规则中的序列的总步骤数
+            int totalSteps = userActionSequenceParams.size();
+
+            // 取出规则中序列条件的时间窗口起止点
             long rangeStart = userActionSequenceParams.get(0).getRangeStart();
             long rangeEnd = userActionSequenceParams.get(0).getRangeEnd();
 
-            // 如果条件的时间窗口起始点>分界点，则在state中查询
+
+            /****************************
+             * 开始分路控制，有如下3中可能性：
+             * a. 只查近期
+             * b. 只查远期
+             * c. 跨界查询
+             * *************************/
+            /**
+             * a. 只查近期
+             * 如果条件的时间窗口起始点>分界点，则在state中查询
+             */
             if(rangeStart>=splitPoint){
                 boolean b = userActionSequenceQueryStateService.queryActionSequence("", eventState, ruleParam);
                 return b;
             }
 
-
-            // 如果条件的时间窗口结束点<分界点，则在clickhouse中查询
+            /**
+             * b.只查远期
+             * 如果条件的时间窗口结束点<分界点，则在clickhouse中查询
+             */
             else if(rangeEnd<splitPoint){
                 boolean b = userActionSequenceQueryClickhouseService.queryActionSequence(logBean.getDeviceId(), null, ruleParam);
                 return b;
             }
 
-
-            // 否则就是跨界查询
+            /**
+             * c.跨界查询
+             * 如果条件的时间窗口跨越分界点，则进行双路查询
+             */
             else{
-                // 重设时间窗口，先查state碰运气
+                /**
+                 * 1. 先查state碰运气
+                 */
+                // 修改时间窗口
                 modifyTimeRange(userActionSequenceParams,splitPoint,rangeEnd);
-
+                // 执行查询
                 boolean b = userActionSequenceQueryStateService.queryActionSequence(logBean.getDeviceId(), eventState, ruleParam);
                 if(b) return true;
 
-                // 如果运气没碰上，则按正统思路查（先查clickhouse，再查state，再整合结果）
-                // 更新一下时间分段
+                //
+
+                /**
+                 * 2. 如果运气没碰上，则按正统思路查（先查clickhouse，再查state，再整合结果）
+                 * 条件如：[A    B     C]
+                 */
+                // 修改时间窗口
                 modifyTimeRange(userActionSequenceParams,rangeStart,splitPoint);
-                // 交给clickhouse service去查远期部分
-                // 条件如： [A    B     C]
+                // 执行clickhouse查询
                 boolean b1 = userActionSequenceQueryClickhouseService.queryActionSequence(logBean.getDeviceId(), eventState, ruleParam);
                 int farMaxStep = ruleParam.getUserActionSequenceQueriedMaxStep();
                 if(b1) return true;
 
-                // 如果远期部分不足以满足整个条件，则将条件截短
-                // 更新一下时间分段
+
+                /**
+                 * 3. 如果远期部分不足以满足整个条件，则将条件截短
+                 */
+                // 修改时间窗口
                 modifyTimeRange(userActionSequenceParams,splitPoint,rangeEnd);
-                // 截短序列
+                // 截短条件序列
                 ruleParam.setUserActionSequenceParams(userActionSequenceParams.subList(farMaxStep,userActionSequenceParams.size()));
-                // 查询
+                // 执行state查询
                 boolean b2 = userActionSequenceQueryStateService.queryActionSequence(logBean.getDeviceId(), eventState, ruleParam);
                 int nearMaxStep = ruleParam.getUserActionSequenceQueriedMaxStep();
                 return farMaxStep+nearMaxStep>=totalSteps;
