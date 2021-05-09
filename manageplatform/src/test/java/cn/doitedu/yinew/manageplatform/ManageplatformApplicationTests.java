0ba66d9f3eae23b1218d061dbdaa66426646c27d
coderblack <coderblack@163.com>
2021-05-09 22:15:47 +0800

    管理后台代码添加

---
+++ manageplatform/src/test/java/cn/doitedu/yinew/manageplatform/ManageplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.doitedu.yinew.manageplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageplatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
